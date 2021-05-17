28a717c28acfe00843ffebcd9dd0906e39014299
Artyom Butin <timoha0703@mail.ru>
2021-05-17 13:33:37 +0300

    added server sock

--- cmd/server.go
+++ cmd/server.go
@@ -1,32 +1,85 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "strings" // требуется только ниже для обработки примера
+import (
+	_ "bytes"
+	"log"
+	"net"
+
+	"github.com/gobwas/ws"
+)
 
 func main() {
-
-	fmt.Println("Launching server...")
-
-	// Устанавливаем прослушивание порта
-	ln, _ := net.Listen("tcp", ":8081")
-
-	// Открываем порт
-	conn, _ := ln.Accept()
-
-	// Запускаем цикл
+	ln, err := net.Listen("tcp", "localhost:1100")
+	if err != nil {
+		// handle error
+	}
+	e := wsflate.Extension{
+		// We are using default parameters here since we use
+		// wsflate.{Compress,Decompress}Frame helpers below in the code.
+		// This assumes that we use standard compress/flate package as flate
+		// implementation.
+		Parameters: wsflate.DefaultParameters,
+	}
+	u := ws.Upgrader{
+		Negotiate: e.Negotiate,
+	}
 	for {
-		// Будем прослушивать все сообщения разделенные \n
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		// Распечатываем полученое сообщение
-		fmt.Print("Message Received:", message)
-		// Процесс выборки для полученной строки
-		newMessage := strings.ToUpper(message)
-		// Отправить новую строку обратно клиенту
-		_, err := conn.Write([]byte(newMessage + "\n"))
+		conn, err := ln.Accept()
 		if err != nil {
-			return
+			log.Fatal(err)
 		}
+
+		// Reset extension after previous upgrades.
+		e.Reset()
+
+		_, err = u.Upgrade(conn)
+		if err != nil {
+			log.Printf("upgrade error: %s", err)
+			continue
+		}
+		if _, ok := e.Accepted(); !ok {
+			log.Printf("didn't negotiate compression for %s", conn.RemoteAddr())
+			conn.Close()
+			continue
+		}
+
+		go func() {
+			defer conn.Close()
+			for {
+				frame, err := ws.ReadFrame(conn)
+				if err != nil {
+					// Handle error.
+					return
+				}
+
+				frame = ws.UnmaskFrameInPlace(frame)
+
+				if wsflate.IsCompressed(frame.Header) {
+					// Note that even after successful negotiation of
+					// compression extension, both sides are able to send
+					// non-compressed messages.
+					frame, err = wsflate.DecompressFrame(frame)
+					if err != nil {
+						// Handle error.
+						return
+					}
+				}
+
+				// Do something with frame...
+
+				ack := ws.NewTextFrame([]byte("this is an acknowledgement"))
+
+				// Compress response unconditionally.
+				ack, err = wsflate.CompressFrame(ack)
+				if err != nil {
+					// Handle error.
+					return
+				}
+				if err = ws.WriteFrame(conn, ack); err != nil {
+					// Handle error.
+					return
+				}
+			}
+		}()
 	}
 }
